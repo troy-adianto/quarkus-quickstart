66177cca5bfab55a38a7cbf40cb572b2abf19b75
admin <admin@che>
2023-03-14 03:11:54 +0000

    another update

--- src/main/java/org/tadianto/MyLivenessCheck.java
+++ src/main/java/org/tadianto/MyLivenessCheck.java
@@ -9,7 +9,7 @@ public class MyLivenessCheck implements HealthCheck {
 
     @Override
     public HealthCheckResponse call() {
-        return HealthCheckResponse.up("Alive");
+        return HealthCheckResponse.up("git");
     }
 
 }
